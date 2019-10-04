f9e86dfe25cd30f6cd09e7697ab63446c43660f8
ccjunta <16j1282@moon.aichi-u.ac.jp>
2019-10-04 08:46:09 +0800

    登録画面ぽいレイアウト作成、場所間違えた

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,7 +2,60 @@
 @section('title','登録画面')
 
 @section('content')
-    <div class="container">
-    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header"></div>
+
+                <div class="card-body">
+                    <form method="POST" action="">
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">名前</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="" name="name" value="" required autofocus>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メール</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="" name="email" value="" required>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="" name="password" required>
+
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">パスワード確認</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">登録
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 @endsection
